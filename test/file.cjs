f1d1fe0c534ec241a364418c611f54d5806ad841
morganney <morganney@gmail.com>
2024-05-24 19:32:05 -0500

    chore: rename package.

--- test/file.cjs
+++ test/file.cjs
@@ -1,5 +1,5 @@
 const { stdout } = require('node:process')
-const { moduleType } = require('module-type')
+const { moduleType } = require('node-module-type')
 const main = async () => {
   stdout.write(await moduleType())
 }
